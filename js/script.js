a104f426e93cc9737239d64cd7831d0778e29f20
alish <kc.alish107@gmail.com>
2023-07-02 03:18:24 -0700

    hh

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ videoList.forEach(vid =>{
       let src = vid.querySelector('.list-video').src;
       let title = vid.querySelector('.list-title').innerHTML;
       document.querySelector('.main-video-container .main-video').src = src;
+      
       document.querySelector('.main-video-container .main-video').play();
       document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
    };
